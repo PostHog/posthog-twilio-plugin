94abcc0641402e08de78a6c29be6764241256fdf
Himanshu Garg <garg_himanshu@outlook.com>
2022-03-02 21:40:49 +0530

    feat: Improvments as per #2

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,43 +2,40 @@ const { createEvent } = require('@posthog/plugin-scaffold/test/utils.js')
 const { setupPlugin, add } = require('../index')
 
 test('test setting negative value for timeout in Twilio', async () => {
-    const response = await setupPlugin({
-        config: {
-            accountSID: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-            authToken: 'your_auth_token',
-            senderPhoneNumber: '+15555555555',
-            triggeringEventsAndNumber: 'event1:+15555555555,event2:+15555555555',
-            timeout: -1,
-        },
-        global: {},
-    })
-    expect(response).toEqual('timeout is not supported')
+    try {
+        const response = await setupPlugin({
+            config: {
+                accountSID: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                authToken: 'your_auth_token',
+                senderPhoneNumber: '+15555555555',
+                triggeringEventsAndNumber: 'event1:+15555555555,event2:+15555555555',
+                timeout: -1,
+            },
+            global: {},
+        })
+        // Fail test if above expression doesn't throw anything.
+      expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe('timeout is not supported')
+    }
 })
 
 test('test setting greater value for timeout in Twilio', async () => {
-    const response = await setupPlugin({
-        config: {
-            accountSID: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-            authToken: 'your_auth_token',
-            senderPhoneNumber: '+15555555555',
-            triggeringEventsAndNumber: 'event1:+15555555555,event2:+15555555555',
-            timeout: 366 * 60 * 60 * 24 + 1,
-        },
-        global: {},
-    })
-    expect(response).toEqual('timeout is not supported')
-})
-
-test('test settings equal value for timeout in Twilio', async () => {
-    const response = await setupPlugin({
-        config: {
-            accountSID: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-            authToken: 'your_auth_token',
-            senderPhoneNumber: '+15555555555',
-            triggeringEventsAndNumber: 'event1:+15555555555,event2:+15555555555',
-            timeout: 365 * 60 * 60 * 24,
-        },
-        global: {},
-    })
-    expect(response).toEqual('Twilio setup successfully')
+    try {
+        const response = await setupPlugin({
+            config: {
+                accountSID: 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                authToken: 'your_auth_token',
+                senderPhoneNumber: '+15555555555',
+                triggeringEventsAndNumber: 'event1:+15555555555,event2:+15555555555',
+                timeout: 366 * 60 * 60 * 24 + 1,
+            },
+            global: {},
+            
+        })
+        // Fail test if above expression doesn't throw anything.
+      expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe('timeout is not supported')
+    }
 })
